e20141dc62af01c1a3cd685a4e49abbc855b9937
mrinalmriyo <mrinalmni@gmail.com>
2019-11-04 11:43:27 +0530

    Modified UX of Launch Screen

--- app/src/main/java/oncreate/apps/Mathest/LaunchScreen.java
+++ app/src/main/java/oncreate/apps/Mathest/LaunchScreen.java
@@ -4,18 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.ImageView;
 
 public class LaunchScreen extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_screen);
-    }
 
-    public void launchApp(View view){
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
+        imageView = findViewById(R.id.logoImageView);
+        imageView.animate().rotationY(360).setDuration(1000);
+
+        CountDownTimer countDownTimer=  new CountDownTimer(1000,1000){
+
+            public void onTick(long millisecondsUntilDone){
+
+            }
+
+            public void onFinish(){
+
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+
+        };
+        countDownTimer.start();
+
     }
 }
