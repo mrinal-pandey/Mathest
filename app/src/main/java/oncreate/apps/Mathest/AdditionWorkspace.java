84c0cd268c5fc80a8579b5f26824555b0967f96d
pchattopadhyay <pritias81@gmail.com>
2019-11-26 22:03:50 +0530

    Addition workspace fixes

--- app/src/main/java/oncreate/apps/Mathest/AdditionWorkspace.java
+++ app/src/main/java/oncreate/apps/Mathest/AdditionWorkspace.java
@@ -7,7 +7,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -88,6 +90,8 @@ public class AdditionWorkspace extends AppCompatActivity {
     int[] number1Array = {0, 0, 0};
     int[] number2Array = {0, 0, 0};
     int[] resultArray = {0, 0, 0, 0};
+    EditText carry1, carry2, carry3, carry4, number11, number12, number13,
+            number21, number22, number23, answer1, answer2, answer3, answer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,6 +109,20 @@ public class AdditionWorkspace extends AppCompatActivity {
 
         Downloader downloader = new Downloader();
         downloader.execute("https://mathest.herokuapp.com/addition-working?uid="+UID+"&number1="+number1+"&number2="+number2);
+        carry1 = findViewById(R.id.carry_digit_1);
+        carry2 = findViewById(R.id.carry_digit_2);
+        carry3 = findViewById(R.id.carry_digit_3);
+        carry4 = findViewById(R.id.carry_digit_4);
+        number11 = findViewById(R.id.number1_digit1);
+        number12 = findViewById(R.id.number1_digit2);
+        number13 = findViewById(R.id.number1_digit3);
+        number21 = findViewById(R.id.number2_digit1);
+        number22 = findViewById(R.id.number2_digit2);
+        number23 = findViewById(R.id.number2_digit3);
+        answer1 = findViewById(R.id.answer_digit1);
+        answer2 = findViewById(R.id.answer_digit2);
+        answer3 = findViewById(R.id.answer_digit3);
+        answer4 = findViewById(R.id.answer_digit4);
     }
 
     public void questionEnable(View view){
@@ -116,5 +134,19 @@ public class AdditionWorkspace extends AppCompatActivity {
         view.setBackground(getDrawable(R.drawable.answer_digit_enable));
         view.setFocusableInTouchMode(true);
         view.requestFocus();
+    }
+
+    public void checkBlocks(View view){
+        if(answerNotEntered()){
+            Toast.makeText(this, "Please enter an answer!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+    public boolean answerNotEntered(){
+        if(answer1.getText().toString() == "" && answer2.getText().toString() == "" &&
+                answer3.getText().toString() == "" && answer4.getText().toString() == ""){
+            return true;
+        }
+        return false;
     }
 }
