2a73597fd5cb38c6d66ad62bed16d68850b1364b
mrinalmriyo <mrinalmni@gmail.com>
2019-11-26 11:05:35 +0530

    Removed duplicate UID generation

--- app/src/main/java/oncreate/apps/Mathest/Signup.java
+++ app/src/main/java/oncreate/apps/Mathest/Signup.java
@@ -17,6 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -25,6 +27,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import oncreate.apps.Mathest.UI.DialogHandler;
@@ -111,14 +115,38 @@ public class Signup extends AppCompatActivity {
     int grade;
     boolean userAdded = false;
     DialogHandler dialogHandler;
+    List<String> listOfUIDs;
+
+    public void UIDValidator(final String UIDGenerated){
+        listOfUIDs = new ArrayList<>();
+        firestoreDatabase.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        listOfUIDs.add(document.getId());
+                    }
+                    for(String uid : listOfUIDs){
+                        if(uid.equals(UIDGenerated)){
+                            UIDGenerator();
+                        }
+                    }
+                    UID_disp.setText("Your UID is: " + UID);
+                }
+            }
+        });
+    }
+
+    public void UIDGenerator(){
+        UID = String.valueOf((int)((Math.random() * 999) + 1));
+        UIDValidator(UID);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
         firestoreDatabase = FirebaseFirestore.getInstance();
-        Random r = new Random();
-        UID = String.valueOf(r.nextInt(999));
 
         UID_disp = findViewById(R.id.UID_disp);
         nameEdit = findViewById(R.id.name_edit);
@@ -126,8 +154,7 @@ public class Signup extends AppCompatActivity {
         gradeEdit = findViewById(R.id.grade_edit);
         dialogHandler = new DialogHandler(this);
 
-        UID_disp.setText("Your UID is: " + UID);
-
+        UIDGenerator();
     }
 
     public void submitDetails(View view) {
