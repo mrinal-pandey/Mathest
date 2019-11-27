c1fa31b565664f1a8ca849ae20c953093fce65a2
mrinalmriyo <mrinalmni@gmail.com>
2019-11-27 14:05:07 +0530

    Displayed keyboard only when EditText receives focus

--- app/src/main/java/oncreate/apps/Mathest/TestPage.java
+++ app/src/main/java/oncreate/apps/Mathest/TestPage.java
@@ -1,5 +1,6 @@
 package oncreate.apps.Mathest;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
