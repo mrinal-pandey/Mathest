f19597cec1939f5598b6efc786846ba55cdcf95f
pchattopadhyay <pritias81@gmail.com>
2019-11-30 12:58:03 +0530

    Adding multiplication workspace design

---
+++ app/src/main/java/oncreate/apps/Mathest/MultiplicationWorkspace.java
@@ -0,0 +1,14 @@
+package oncreate.apps.Mathest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiplicationWorkspace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplication_workspace);
+    }
+}
