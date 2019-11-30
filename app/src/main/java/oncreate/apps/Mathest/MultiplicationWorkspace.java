ae8e75b6164f7f50a0b8fa2eb3918629667cba99
pchattopadhyay <pritias81@gmail.com>
2019-11-30 13:00:27 +0530

    Adding edit text color change response in multiplication workspace

--- app/src/main/java/oncreate/apps/Mathest/MultiplicationWorkspace.java
+++ app/src/main/java/oncreate/apps/Mathest/MultiplicationWorkspace.java
@@ -3,6 +3,7 @@ package oncreate.apps.Mathest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MultiplicationWorkspace extends AppCompatActivity {
 
@@ -11,4 +12,15 @@ public class MultiplicationWorkspace extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplication_workspace);
     }
+    public void questionEnable(View view){
+        view.setBackground(getResources().getDrawable(R.drawable.question_digit_enable));
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+    }
+    public void answerEnable(View view){
+        view.setBackground(getResources().getDrawable(R.drawable.answer_digit_enable));
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+    }
+
 }
